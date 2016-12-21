f880144cac706326c106413d3580a48c3fade337
Ben Fung <befung@gmail.com>
2016-12-20 22:48:34 -0500

    fixed syntax and add findPaths function.

--- dyCd.py
+++ dyCd.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python
 
-import angr
 import sys
+import angr
 
 
 class Menu():
     def __init__(self):
         self.menuItems = {}
-        self.add_item(0,'exit',self._exit)
+        self.add_item(0, 'exit', self._exit)
 
     def _exit(self):
         return False
 
-    def add_item(self,key,value,func):
-        funcObj = (value,func)
-        self.menuItems[key] = funcObj 
+    def add_item(self, key, value, func):
+        funcObj = (value, func)
+        self.menuItems[key] = funcObj
 
     def get_choice(self):
         for f in self.menuItems:
-            print '%d : %s' % ( f, self.menuItems[f][0])
-        return int( raw_input("Please enter : " )) 
+            print '%d : %s' % (f, self.menuItems[f][0])
+        return int(raw_input("Please enter : "))
 
 
 class Target(Menu):
@@ -30,24 +30,27 @@ class Target(Menu):
         self._mFuncList = dict(self._mProj.kb.functions)
         self.paths = []
         Menu.__init__(self)
-        self.add_item(1,'list functions', self.display_functions)
-        self.add_item(2,'analyse a function paths',self.analyse_func_paths)
+        self.add_item(1, 'list functions', self.display_functions)
+        self.add_item(2, 'analyse a function paths', self.analyse_func_paths)
+
+    def findPaths(self, p):
+        pp = p.step()
+        if len(pp) == 0:
+            self.paths.append(p)
 
     def analyse_func_paths(self):
-        addr = 0
         f_addr = 0
         try:
-            f_addr = int( raw_input("Please enter a function address in hex format: " ),16)
+            f_addr = int(raw_input("Please enter a function address in hex format: "), 16)
             print 'Start analyzing for 0x%x ...' % (f_addr)
-        except ValueError, e :
-            print ("'%s' is not a valid function address" % e.args[0].split(": ")[1])
+        except ValueError, e:
+            print "'%s' is not a valid function address" % e.args[0].split(": ")[1]
             return True
-    
+
         state = self._mProj.factory.blank_state(addr=f_addr)
         print 'Processing...'
 
         path = self._mProj.factory.path(state)
-        #pg = self._mProj.factory.path_group(state)
         if path.addr != f_addr:
             print 'for some reason, the path address is not correct.'
 
@@ -56,10 +59,10 @@ class Target(Menu):
     def display_functions(self):
         for f in self._mFuncList:
             s = self._mFuncList[f]
-            print 'Address [%#x] \t%s ' % ( s.addr, s.name)
+            print 'Address [%#x] \t%s ' % (s.addr, s.name)
         return True
 
-    def execute_cmd(self,cmd):
+    def execute_cmd(self, cmd):
         if cmd in self.menuItems:
             return self.menuItems[cmd][1]()
         else:
@@ -72,29 +75,23 @@ def show_menu(pTarget):
         try:
             choice = pTarget.get_choice()
             is_valid = 1
-        except ValueError, e :
-            print ("'%s' is an invalid input" % e.args[0].split(": ")[1])
-    return pTarget.execute_cmd(choice) 
-    
+        except ValueError, e:
+            print "'%s' is an invalid input" % e.args[0].split(": ")[1]
+    return pTarget.execute_cmd(choice)
 
-        
 def execute(file):
-    print 'Provide binary: ' + file 
-    #proj = angr.Project(file, load_options={'auto_load_libs': False})
-    #cfg = proj.analyses.CFG()
-    #b_funcs = dict(proj.kb.functions)
+    print 'Provide binary: ' + file
 
     rc = True
     target = Target(file)
 
-    while rc == True:
-        #if menu(b_funcs) < 0:
+    while rc:
         rc = show_menu(target)
     print 'Good bye!'
 
 
 if __name__ == '__main__':
-    if( len(sys.argv) < 2):
+    if len(sys.argv) < 2:
         print "Please provide a binary file"
     else:
         execute(sys.argv[1])
